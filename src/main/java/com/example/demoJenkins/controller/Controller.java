788dedd639645e4359119b18018c6502808bc834
Haritha <haritha@gmail.com>
2023-05-22 14:33:09 +0530

    first commit

--- src/main/java/com/example/demoJenkins/controller/Controller.java
+++ src/main/java/com/example/demoJenkins/controller/Controller.java
@@ -1,2 +1,20 @@
-package com.example.demoJenkins.controller;public class Controller {
+package com.example.demoJenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Controller {
+    @GetMapping("/welcome")
+    public String welcom() {
+        return "welcome";
+    }
+    @GetMapping("/login")
+    public String log(){
+        return "login";
+    }
+
+
 }
